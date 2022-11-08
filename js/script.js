664bd434622460f46cfc8da24b4de37ea7fc245c
rsahmed16 <112486837+rsahmed16@users.noreply.github.com>
2022-11-08 11:52:35 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function init(){
+  //add your javascript between these two lines of code
+  function alertFunction(){
+    input = document.getElementById('entryinput')
+    alert('Rabia Ahmed: ' + inputForm.value)
+    document.getElementById('textoutput').innerHTML = input.value;
+  }
+
+  // This is the button
+  var button=document.getElementById('entrybutton');
+  button.addEventListener("click",updateText);
+
+}
+window.addEventListener('load', init);

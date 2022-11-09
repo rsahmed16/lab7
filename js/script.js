212c57b0f33c615d2cfbeaf86f8b7ae133dd1f12
rsahmed16 <112486837+rsahmed16@users.noreply.github.com>
2022-11-08 18:35:33 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-function init(){
-  function alertMe(){
+function init() {
+  //add your javascript between these two lines of code
+  function alertFunction() {
     var input = document.getElementById('entryinput').value;
 	document.getElementById('textoutput').innerHTML = input;
 	alert('Rabia Sajjad Ahmed: ' + input);
   }
-  
+  // This will add the button
   var button = document.getElementById('entrybutton');
-  button.addEventListener('click', alertMe);
+  button.addEventListener('click', alertFunction);
 }
 window.addEventListener('load', init);
